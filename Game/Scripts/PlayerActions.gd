f2a5ae828ce3ffc27cf0a817dac30f89e15aac4b
AAZZAZRON <aaronzhu3.1415@gmail.com>
2021-12-27 14:21:04 -0500

    move

--- Game/Scripts/PlayerActions.gd
+++ Game/Scripts/PlayerActions.gd
@@ -1,2 +1,25 @@
 extends KinematicBody2D
 
+var speed : int = 200
+var vel : Vector2 = Vector2()
+
+onready var sprite : Sprite = get_node("Sprite")
+
+func _physics_process(delta):
+	vel.x = 0
+	vel.y = 0
+	
+	if Input.is_action_pressed("move_left"):
+		vel.x = -speed
+	elif Input.is_action_pressed("move_right"):
+		vel.x = speed
+	elif Input.is_action_pressed("move_up"):
+		vel.y = -speed
+	elif Input.is_action_pressed("move_down"):
+		vel.y = speed
+	
+	vel = move_and_slide(vel, Vector2.UP)
+	
+
+
+
