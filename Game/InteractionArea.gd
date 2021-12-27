40fbb422c898ac88bde9a4bd3c4b4a22dbad7597
Maplefin <james.huynh444@gmail.com>
2021-12-27 16:42:10 -0500

    added dialogue, made it so you cant move while speaking

--- Game/InteractionArea.gd
+++ Game/InteractionArea.gd
@@ -1,6 +1,9 @@
 extends Area2D
 
 var inside = false
+var speaking = false
+signal lock
+signal unlock
 
 func _on_InteractionArea_body_entered(body):
 	inside = true
@@ -11,5 +14,12 @@ func _on_InteractionArea_body_exited(body):
 	$Notif.visible = false
 
 func _input(event):
-	if Input.is_action_just_pressed("interact") && inside:
-		print("hello!")
+	if Input.is_action_just_pressed("interact") && inside && !speaking:
+		speaking = true
+		emit_signal("lock")
+		var dialogue = Dialogic.start('Test')
+		add_child(dialogue)
+		yield(dialogue, "dialogic_signal")
+		yield(get_tree().create_timer(0.2),"timeout")
+		speaking = false
+		emit_signal("unlock")
--- Game/Scripts/PlayerActions.gd
+++ Game/Scripts/PlayerActions.gd
@@ -2,23 +2,26 @@ extends KinematicBody2D
 
 var speed = 200
 var vel = Vector2()
+var locked = false
 
 onready var sprite : Sprite = get_node("Sprite")
 
 func _physics_process(delta):
-	vel = vel.normalized()
-	
-	if Input.is_action_pressed("move_left"):
-		vel.x = -speed
-	if Input.is_action_pressed("move_right"):
-		vel.x = speed
-	if Input.is_action_pressed("move_up"):
-		vel.y = -speed
-	if Input.is_action_pressed("move_down"):
-		vel.y = speed
-	
-	vel = move_and_slide(vel, Vector2.UP)
-	
+	if !locked:
+		vel = vel.normalized()
+		if Input.is_action_pressed("move_left"):
+			vel.x = -speed
+		if Input.is_action_pressed("move_right"):
+			vel.x = speed
+		if Input.is_action_pressed("move_up"):
+			vel.y = -speed
+		if Input.is_action_pressed("move_down"):
+			vel.y = speed
+		vel = move_and_slide(vel, Vector2.UP)
+
+func _on_InteractionArea_lock():
+	locked = true
 
 
-
+func _on_InteractionArea_unlock():
+	locked = false
