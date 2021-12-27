b163dda190c7c0aa1a5e194c4f1586f69205b1e7
Maplefin <james.huynh444@gmail.com>
2021-12-27 13:42:56 -0500

    added increment and decrement menu for resources

---
+++ Game/Scripts/CoalLabel.gd
@@ -0,0 +1,7 @@
+extends RichTextLabel
+
+
+
+
+func _on_Holder_update(num):
+	text = "Coal: " + String(num)
--- Game/Scripts/InvButton.gd
+++ Game/Scripts/InvButton.gd
@@ -1,5 +1,15 @@
 extends Panel
 
+var resources = 0
+signal update(num)
 
 func _on_Button_pressed():
 	visible = !visible
+
+func _on_Decrement_pressed():
+	resources -= 1
+	emit_signal("update",resources)
+
+func _on_Increment_pressed():
+	resources += 1
+	emit_signal("update",resources)
