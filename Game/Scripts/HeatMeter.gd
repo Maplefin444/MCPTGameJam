bf320abc8dd597381b2d12f4de84648d17f6962c
Maplefin <james.huynh444@gmail.com>
2021-12-28 10:27:20 -0500

    made heatmeter slow down over time
    user experience:tm:

--- Game/Scripts/HeatMeter.gd
+++ Game/Scripts/HeatMeter.gd
@@ -4,6 +4,7 @@ var decay = 0.1
 var build = 0.5
 var retain_level = 1
 var retaining = true
+var slowdown = 1
 
 func _ready():
 	value = 100
@@ -13,13 +14,15 @@ func _physics_process(delta):
 		yield(get_tree().create_timer(retain_level*3),"timeout")
 		retaining = false
 	if not heated && not retaining:
-		value -= decay
+		slowdown += 0.001
+		value -= decay/slowdown
 	elif heated:
 		value += decay
 
 
 func _on_GUI_enter():
 	heated = true
+	slowdown = 1
 
 
 func _on_GUI_exit():
