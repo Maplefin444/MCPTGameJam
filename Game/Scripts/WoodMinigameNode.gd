d1ff087d0f063014b62c7419475ece3c9960f4bc
Maplefin <james.huynh444@gmail.com>
2021-12-28 11:27:54 -0500

    added a bit more functionality to wood minigame

--- Game/Scripts/WoodMinigameNode.gd
+++ Game/Scripts/WoodMinigameNode.gd
@@ -2,12 +2,14 @@ extends CanvasLayer
 
 var y = 96
 var dir = true
+var count = 0
+var speed = 15
 
 func _physics_process(delta):
 	if dir:
-		y+=5
+		y+=speed
 	else:
-		y-=5
+		y-=speed
 	
 	if y > get_viewport().size.y - 96:
 		dir = false
@@ -15,9 +17,14 @@ func _physics_process(delta):
 		dir = true
 	$HitObject/Sprite.position = Vector2(736,y)
 	$HitObject/CollisionShape2D.position = Vector2(736,y)
+	if count == 3:
+		print("win")
 
 func _input(InputEvent):
 	if Input.is_action_just_pressed("interact") && len($HitObject.get_overlapping_areas()) == 1:
-		print("wow")
+		count += 1
+		$Label.visible = true
+		yield(get_tree().create_timer(0.5), "timeout")
+		$Label.visible = false
 	elif Input.is_action_just_pressed("interact"):
-		print("cringe")
+		count = 0
