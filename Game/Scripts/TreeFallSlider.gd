470ceb2553cc9febee859ce3db463621799c9682
AAZZAZRON <aaronzhu3.1415@gmail.com>
2021-12-29 17:25:07 -0500

    cloth game final

--- Game/Scripts/TreeFallSlider.gd
+++ Game/Scripts/TreeFallSlider.gd
@@ -6,4 +6,4 @@ func _on_HSlider_mouse_exited() -> void:
 	self.release_focus()
 
 func _physics_process(delta):
-	$Guess.text = "You guessed: " + String((value * 1.2) + 240)
+	$Guess.text = "You guessed: " + String((value * 5) + 500) + "cm"
