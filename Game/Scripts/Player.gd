08c5d462073e4fdfadd848dacf2b589d4fe0aaf4
Maplefin <james.huynh444@gmail.com>
2021-12-31 09:37:50 -0500

    fixed up minimap

--- Game/Scripts/Player.gd
+++ Game/Scripts/Player.gd
@@ -1,4 +1,8 @@
 extends ColorRect
 
 func _physics_process(delta):
-	set_position(Vector2((get_parent().playerpos.x/75)+50,(get_parent().playerpos.y/75)+50))
+	set_position(Vector2((get_parent().get_parent().playerpos.x/75)+50,(get_parent().get_parent().playerpos.y/75)+50))
+	if get_position().x > 106 or get_position().y > 106 or get_position().x < 0 or get_position().y < 0:
+		visible = false
+	else:
+		visible = true
